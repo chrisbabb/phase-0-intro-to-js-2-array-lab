0b430c5f05ae139c8d2c19877566437115941d20
Christopher Babb <chris@appsoftdev.com>
2022-06-08 10:33:21 -0700

    commit tree 653ec7ff012e80e455c9edc3ce594befa97e64ed

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    let newCats = [...cats, name]
+    return newCats
+}
+
+function prependCat(name) {
+    let newCats = [name, ...cats]
+    return newCats
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
